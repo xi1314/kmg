c6f66d568e1c030c0a82fd2188d28c36945ce47c
bronze1man <bronze1man@gmail.com>
2014-08-16 12:54:13 +0800

    save

--- kmgCrypto/hash.go
+++ kmgCrypto/hash.go
@@ -2,9 +2,15 @@ package kmgCrypto
 
 import (
 	"crypto/md5"
+	"crypto/sha256"
 	"crypto/sha512"
 	"encoding/hex"
 )
+
+func Sha256Hex(data []byte) string {
+	out := sha256.Sum256(data)
+	return hex.EncodeToString(out[:])
+}
 
 func Sha512Hex(data []byte) string {
 	out := sha512.Sum512(data)
