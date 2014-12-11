987b80931a5c73bcdf622a682ad199ade758f7e1
bronze1man <bronze1man@gmail.com>
2014-12-12 02:22:46 +0800

    save

---
+++ kmgNet/Server.go
@@ -0,0 +1,42 @@
+package kmgNet
+
+import (
+	"net"
+	"strconv"
+)
+
+type Server interface {
+	//异步开启服务器
+	Start() error
+	//关闭服务器,通常需要等一段时间(1ms)来等服务器确实关闭了
+	Close() error
+	//监听地址,如果可能请返回net库里面有的Addr,如果还没有开始监听会panic
+	Addr() (net.Addr, error)
+}
+
+func MustGetServerAddrString(s Server) string {
+	addr, err := s.Addr()
+	if err != nil {
+		panic(err)
+	}
+	return addr.String()
+}
+
+func MustGetServerLocalAddrString(s Server) string {
+	addr, err := s.Addr()
+	if err != nil {
+		panic(err)
+	}
+	port, err := PortFromNetAddr(addr)
+	if err != nil {
+		panic(err)
+	}
+	return "127.0.0.1:" + strconv.Itoa(port)
+}
+
+func MustServerStart(s Server) {
+	err := s.Start()
+	if err != nil {
+		panic(err)
+	}
+}
