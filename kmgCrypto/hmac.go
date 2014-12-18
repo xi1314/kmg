305894039f828245236280e0ad5fb9989866476d
bronze1man <bronze1man@gmail.com>
2014-12-19 00:41:15 +0800

    save

--- kmgCrypto/hmac.go
+++ kmgCrypto/hmac.go
@@ -1,11 +1,12 @@
 package kmgCrypto
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
-	"hash/adler32"
 	"encoding/binary"
-	"bytes"
+	"hash/adler32"
+	"hash"
 )
 
 //长度32字节
@@ -23,13 +24,29 @@ func GetSha256MAC(message, key []byte) []byte {
 
 //长度4字节 速度非常快 1GB/s
 func CheckAdler32(message, messageMAC []byte) bool {
-	get:=GetAdler32(message)
-	return bytes.Equal(get,messageMAC)
+	get := GetAdler32(message)
+	return bytes.Equal(get, messageMAC)
 }
 
-func GetAdler32(message []byte) []byte{
-	outi:= adler32.Checksum(message)
-	out:=make([]byte,4)
-	binary.LittleEndian.PutUint32(out,outi)
+func GetAdler32(message []byte) []byte {
+	outi := adler32.Checksum(message)
+	out := make([]byte, 4)
+	binary.LittleEndian.PutUint32(out, outi)
 	return out
+}
+
+//长度4字节 速度非常快 1GB/s
+func CheckAdler32Mac(message, messageMAC,key []byte) bool {
+	get := GetAdler32Mac(message,key)
+	return hmac.Equal(get, messageMAC)
+}
+
+func GetAdler32Mac(message,key []byte) []byte {
+	mac := hmac.New(newAdler32, key)
+	mac.Write(message)
+	return mac.Sum(nil)
+}
+
+func newAdler32()hash.Hash{
+	return adler32.New()
 }
