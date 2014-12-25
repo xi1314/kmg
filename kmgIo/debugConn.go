c2400e5e7d5f132612dcaf3313df125480ac4345
bronze1man <bronze1man@gmail.com>
2014-12-25 18:59:47 +0800

    save

--- kmgIo/debugConn.go
+++ kmgIo/debugConn.go
@@ -3,6 +3,8 @@ package kmgIo
 import (
 	"fmt"
 	"io"
+	"sync/atomic"
+	"time"
 )
 
 //debug a io.ReadWriteCloser
@@ -45,4 +47,52 @@ func (c debugRwc) Close() (err error) {
 	err = c.ReadWriteCloser.Close()
 	fmt.Println("[debugConn]", c.Name, "Close finish err:", err)
 	return err
+}
+
+type sumSizeRwc struct {
+	io.ReadWriteCloser
+	Name       string
+	startTime  time.Time
+	writeBytes uint64
+	readBytes  uint64
+	readNum    uint64
+	writeNum   uint64
+	hasClose   bool
+}
+
+func (c *sumSizeRwc) Write(b []byte) (n int, err error) {
+	n, err = c.ReadWriteCloser.Write(b)
+	if n > 0 {
+		atomic.AddUint64(&c.writeBytes, uint64(n))
+		atomic.AddUint64(&c.writeNum, 1)
+	}
+	return n, err
+}
+
+func (c *sumSizeRwc) Read(b []byte) (n int, err error) {
+	n, err = c.ReadWriteCloser.Read(b)
+	if n > 0 {
+		atomic.AddUint64(&c.readBytes, uint64(n))
+		atomic.AddUint64(&c.readNum, 1)
+	}
+	return n, err
+}
+
+func (c *sumSizeRwc) Close() (err error) {
+	err = c.ReadWriteCloser.Close()
+	if !c.hasClose {
+		fmt.Printf("[sumSizeRwc] [%s] read[bytes:%s num:%d] write[bytes:%s num:%d] duration:%s\n",
+			c.Name, FmtByteNum(int(c.readBytes)), c.readNum, FmtByteNum(int(c.writeBytes)), c.writeNum,
+			time.Since(c.startTime))
+		c.hasClose = true
+	}
+	return err
+}
+
+func NewSumSizeRwc(rwc io.ReadWriteCloser, name string) io.ReadWriteCloser {
+	return &sumSizeRwc{
+		ReadWriteCloser: rwc,
+		Name:            name,
+		startTime:       time.Now(),
+	}
 }
--- kmgNet/RwcConn.go
+++ kmgNet/RwcConn.go
@@ -7,6 +7,24 @@ import (
 	"time"
 )
 
+type RwcOverConn struct {
+	io.Reader
+	io.Writer
+	io.Closer
+	net.Conn
+}
+
+func (c *RwcOverConn) Read(p []byte) (n int, err error) {
+	return c.Reader.Read(p)
+}
+
+func (c *RwcOverConn) Write(p []byte) (n int, err error) {
+	return c.Writer.Write(p)
+}
+func (c *RwcOverConn) Close() (err error) {
+	return c.Closer.Close()
+}
+
 func RwcConn(rwc io.ReadWriteCloser) net.Conn {
 	return rwcConn{
 		ReadWriteCloser: rwc,
