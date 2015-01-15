2db4c3c31294c19c11f3a3235f36fcb481eb9a8b
bronze1man <bronze1man@gmail.com>
2015-01-15 13:29:01 +0800

    save

--- kmgNet/Tfo_linux_test.go
+++ kmgNet/Tfo_linux_test.go
@@ -3,16 +3,14 @@ package kmgNet_test
 import (
 	"github.com/bronze1man/kmg/kmgNet"
 	"github.com/bronze1man/kmg/kmgNet/netTester"
-	"github.com/bronze1man/kmg/kmgTest"
 	"net"
 	"testing"
 )
 
 func TestTfo(ot *testing.T) {
-	t := kmgTest.NewTestTools(ot)
-	l, err := kmgNet.TfoListen("tcp", "127.0.0.1:50000")
-	t.Equal(err, nil)
-	netTester.RunTcpListenerDialerTest(l, func() (net.Conn, error) {
+	netTester.RunTcpListenerDialerTest(func() (net.Listener, error) {
+		return kmgNet.TfoListen("tcp", "127.0.0.1:50000")
+	}, func() (net.Conn, error) {
 		return kmgNet.TfoLazyDial("tcp", "127.0.0.1:50000")
-	})
+	}, false)
 }
