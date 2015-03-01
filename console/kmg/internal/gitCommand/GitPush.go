18d118a427e9a1e7ed5fc02aefec7f1f0e46313b
bronze1man <bronze1man@gmail.com>
2015-03-01 22:38:29 +0800

    'save'

---
+++ console/kmg/internal/gitCommand/GitPush.go
@@ -0,0 +1,24 @@
+package goCommand
+
+import (
+	"github.com/bronze1man/kmg/kmgCmd"
+	"github.com/bronze1man/kmg/kmgConsole"
+)
+
+func init() {
+	kmgConsole.AddAction(kmgConsole.Command{
+		Name:   "GitPush",
+		Desc:   "add,commit,push this git resp",
+		Runner: gitPush,
+	})
+
+}
+
+func gitPush(){
+    err := kmgCmd.NewOsStdioCmdString("git add -A").Run()
+    kmgConsole.ExitOnErr(err)
+    err = kmgCmd.NewOsStdioCmdString("git commit -am'save'").Run()
+    kmgConsole.ExitOnErr(err)
+    err = kmgCmd.NewOsStdioCmdString("git push origin master").Run()
+    kmgConsole.ExitOnErr(err)
+}
--- console/kmg/main.go
+++ console/kmg/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/bronze1man/kmg/kmgConsole"
     _ "github.com/bronze1man/kmg/console/kmg/internal"
     _ "github.com/bronze1man/kmg/console/kmg/internal/goCommand"
+    _ "github.com/bronze1man/kmg/console/kmg/internal/gitCommand"
 )
 
 func main() {
